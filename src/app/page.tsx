b022ef99bbaacf2c8c8f86dc706465074a223fda
Giyoun <109953972+Gi-Youn-Oh@users.noreply.github.com>
2023-04-12 19:44:45 +0900

    version update check (#1)
    * version2
    
    * version3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   console.log(os.hostname()); 
   return (
   <div>
-    <h1>메인 페이지</h1>
+    <h1>메인 페이지 버전 3</h1>
     <Counter />
     <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" alt="shop" width={400} height={400} />
   </div>
